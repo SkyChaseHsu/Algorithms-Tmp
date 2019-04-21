c47983e83010981f0c7c7ef68b2d3091928ae60f
SkyChaseHsu <buhruw-3gyzpU-jykbaw>
2019-04-21 19:05:20 +0800

    commit the change

---
+++ 01_closest_pair/cmake-build-debug/CMakeFiles/closest_pair.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xukexin/GIT_RES/Algorithms-Tmp/01_closest_pair/main.cpp" "/Users/xukexin/GIT_RES/Algorithms-Tmp/01_closest_pair/cmake-build-debug/CMakeFiles/closest_pair.dir/main.cpp.o"
+  "/Users/xukexin/GIT_RES/Algorithms-Tmp/01_closest_pair/point.cpp" "/Users/xukexin/GIT_RES/Algorithms-Tmp/01_closest_pair/cmake-build-debug/CMakeFiles/closest_pair.dir/point.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 01_closest_pair/cmake-build-debug/CMakeFiles/closest_pair.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/closest_pair.dir/main.cpp.o"
+  "CMakeFiles/closest_pair.dir/point.cpp.o"
+  "closest_pair.pdb"
+  "closest_pair"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/closest_pair.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
